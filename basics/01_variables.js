b24919edcf550e796a18c11d7e3fe904f3048b5b
zaindev1 <zain.ali83900@gmail.com>
2024-05-21 12:42:58 +0000

    01_variables

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=84320923
+let accountEmail="dev@gmail.com"
+let accountPassword="passwrd123"
+accountCity="karachi"
+
+
+accountEmail="zaindev@gmail.com"
+accountPassword="zaina221@"
+accountCity="london"
+
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity])
