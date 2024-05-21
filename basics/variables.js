960deaff0b86f390993096d49256792137426c37
zaindev1 <zain.ali83900@gmail.com>
2024-05-21 12:34:58 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+/* const ki value ko change nh kr skty na hi edit kr skty hain*/
+const accountId = 62925
+let accountEmail ="dev@gmail.com"
+let accountPassword="password124"
+accountCity="UAE"
+
+
+accountEmail="zaondeveloper@gmail.com"
+accountPassword="zaindev221@"
+accountCity="karachi"
+
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity])
