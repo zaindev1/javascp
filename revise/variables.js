e2aee816474b6592e873bfd97fba94eae69fcefc
zaindev1 <zain.ali83900@gmail.com>
2024-05-22 11:01:49 +0000

    revision

---
+++ revise/variables.js
@@ -0,0 +1,13 @@
+const accountId=79057430952
+let accountEmail="admin@gmail.com"
+let accountPassword="pakistan221@"
+accountCity="karachi"
+
+console.log(accountId);
+
+accountEmail="zaindev@gmail.com"
+accountPassword="aloneme221@"
+accountCity="Canada"
+
+
+console.table([accountEmail, accountPassword, accountCity])
