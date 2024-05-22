3ecd6f876b9762fd1a97cb95b99fe3db55aa69b3
zaindev1 <zain.ali83900@gmail.com>
2024-05-22 14:16:29 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//only use for update javascript
+
+
+
+let age = 22
+let isLoggedIn = false
+let state = null
+
+//console.log(typeof undefined); typesof undefined
+//console.log(typeof null); typeof object
+
+
+//premetive datatypes
+
+//number =>2 to power 53
+//bigint => when u choose a huge number 
+//string => "" when u use string must use double qouma
+//undefined => value doesnt exist value abh assign nh hui eh  
+//null => null aik object eh aur yeh khud meh aik value eh "stand alone value"
+//boolen => yeh tb use hta eh jahan true aur false ki bat hu rhi hugi maslan han ya naa login ya logout
+//symbol => uniqueness 
+
+
+// object
