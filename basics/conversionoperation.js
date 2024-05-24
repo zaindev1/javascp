d767140c1867e2c0f78f0d4846489ad3181fc0a5
zaindev1 <zain.ali83900@gmail.com>
2024-05-24 12:25:30 +0000

    conversionOperation

---
+++ basics/conversionoperation.js
@@ -0,0 +1,35 @@
+let score ="zain"
+
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber =Number(score)
+
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN "not a number"
+// true => 1 ; false =>0
+
+//practice 
+let bigNumber=4742385238
+let bigIntNumber=BigInt(bigNumber)
+console.log(typeof(bigIntNumber));
+
+
+let uname ="zain ul abideen"
+let number=Number(uname)
+console.log(typeof(number));
+
+
+let string="zain ul abideen"
+let booleanNumber=Boolean (string)
+console.log(booleanNumber);
+
+// 1 => ture
+// 0 => false
+// "" => false
+//"zain" => true
+
